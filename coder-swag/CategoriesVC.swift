54c1e7749c64fb3fc4c342243a49d32f7edd360d
Zsuzsanna Seredi-Szabo <zseredi-@student.42.fr>
2020-04-07 11:32:17 +0200

    Initial Commit

---
+++ coder-swag/CategoriesVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coder-swag
+//
+//  Created by Zsuzsanna Seredi on 07/04/2020.
+//  Copyright © 2020 Zsuzsanna Seredi. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
