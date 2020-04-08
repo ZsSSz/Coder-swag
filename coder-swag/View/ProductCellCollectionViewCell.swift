ed97048107b8387c903c08aa6dd97eb4ec468628
Zsuzsanna Seredi-Szabo <zseredi-@student.42.fr>
2020-04-08 13:16:19 +0200

    Collection view cells.

---
+++ coder-swag/View/ProductCellCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCellCollectionViewCell.swift
+//  coder-swag
+//
+//  Created by Zsuzsanna Seredi on 08/04/2020.
+//  Copyright © 2020 Zsuzsanna Seredi. All rights reserved.
+//
+
+import UIKit
+
+class ProductCellCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
